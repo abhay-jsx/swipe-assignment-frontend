c883ef83b796ec7a43688c03219410e9910c8ff8
abhay-jsx <abhay.jain@cogoport.com>
2023-11-26 15:29:02 +0530

    add read.me

--- src/redux/invoicesSlice.js
+++ src/redux/invoicesSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const invoicesSlice = createSlice({
   name: "invoices",
   initialState: [],
+  
   reducers: {
     addInvoice: (state, action) => {
       state.push(action.payload);
